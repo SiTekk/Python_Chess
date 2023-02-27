c8c3f86953a880e407aa7b1bc9de5991e802b622
Simon <siekie41@googlemail.com>
2023-02-27 22:59:25 +0100

    Added loading of assets.

--- src/main.py
+++ src/main.py
@@ -1,19 +1,40 @@
 import sys, pygame
 
 
+assetsPath = [
+    "assets/black_bishop.svg",
+    "assets/black_king.svg",
+    "assets/black_knight.svg",
+    "assets/black_pawn.svg",
+    "assets/black_queen.svg",
+    "assets/black_rook.svg",
+    "assets/whit_rook.svg",
+    "assets/white_bishop.svg",
+    "assets/white_king.svg",
+    "assets/white_knight.svg",
+    "assets/white_pawn.svg",
+    "assets/white_queen.svg"
+]
+
+
 def main():
     print (pygame.ver)
 
     pygame.init()
 
-    size = width, height = 320, 240
+    size = width, height = 800, 600
+    
     speed = [2, 2]
-    black = 0, 0, 0
+    background = 255, 255, 255
 
-    screen = pygame.display.set_mode(size)
+    screen = pygame.display.set_mode(size, pygame.RESIZABLE, vsync=1)
 
     ball = pygame.image.load("assets/intro_ball.gif")
     ballrect = ball.get_rect()
+
+    assets = []
+    for path in assetsPath:
+        assets.append(pygame.image.load(path))
 
     while True:
         for event in pygame.event.get():
@@ -25,8 +46,12 @@ def main():
         if ballrect.top < 0 or ballrect.bottom > height:
             speed[1] = -speed[1]
 
-        screen.fill(black)
+        screen.fill(background)
         screen.blit(ball, ballrect)
+
+        for asset in assets:
+            screen.blit(asset, asset.get_rect())
+
         pygame.display.flip()
 
 
